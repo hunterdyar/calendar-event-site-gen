5dbbf0f35c683e5143b2c01464e5342d462d0b60
Disuko <64217407+disukomusic@users.noreply.github.com>
2024-02-02 13:23:14 -0500

    hacky workaround for multi-day dates, tries and escapes it

--- generate.py
+++ generate.py
@@ -1,10 +1,10 @@
-#built ins
+# built ins
 import argparse
 import shutil
 from datetime import datetime
 import json
 import urllib.request
-#external deps
+# external deps
 import icalendar
 import chevron
 import arrow
@@ -53,15 +53,20 @@ def render(template, calendar, settings, include_past=False):
 
     for event in calendar.walk('VEVENT'):
         total += 1
-        start = arrow.Arrow.fromdatetime(event.get('DTSTART').dt)
+        ndt = event.get('DTSTART').dt
+        try:
+            start = arrow.Arrow.fromdatetime(ndt)
+        except:
+            continue
+
         if not include_past:
             if start < arrow.utcnow():
                 skipped += 1
                 continue
 
-       # start = start.replace(tzinfo=timezone.utc).astimezone(tz=ZoneInfo(args["timezone"]))
+        # start = start.replace(tzinfo=timezone.utc).astimezone(tz=ZoneInfo(args["timezone"]))
         stamp = arrow.Arrow.fromdatetime(event.get('dtstamp').dt)
-       # stamp = stamp.replace(tzinfo=timezone.utc).astimezone(tz=ZoneInfo(args["timezone"]))
+        # stamp = stamp.replace(tzinfo=timezone.utc).astimezone(tz=ZoneInfo(args["timezone"]))
 
         end = arrow.Arrow.fromdatetime(event.get('DTEND').dt)
         description = event.get('x-alt-desc')
@@ -89,7 +94,7 @@ def render(template, calendar, settings, include_past=False):
             'start_human': start.humanize(),
             'duration': {
                 'human': duration,
-                #'human_no_seconds': humanize.precisedelta(duration, minimum_unit="minutes"),
+                # 'human_no_seconds': humanize.precisedelta(duration, minimum_unit="minutes"),
                 'duration_total_seconds': total_seconds,
                 'hms': str(duration)
             },
@@ -103,6 +108,7 @@ def render(template, calendar, settings, include_past=False):
     out = chevron.render(template, args)
     print(f"Rendered: {rendered}. Skipped: {skipped}. Total: {total} events")
     return out
+
 
 def apply_default_settings(settings):
     if "date_format" not in settings:
@@ -121,8 +127,10 @@ def apply_default_settings(settings):
         settings["timezone"] = ""
     return settings
 
+
 parser = argparse.ArgumentParser(description="Generate Calendar Event Site")
 parser.add_argument("url", help="a public .ics calendar URL to use as a source.")
+
 
 def main():
     # we should use command line arguments for the import settings and output file, so github can use SECRETS to make forking the project even easier.
@@ -132,7 +140,7 @@ def main():
         settings = apply_default_settings(settings)
 
     # get calendar and data
-    output_file = settings["output_folder"]+settings["output_file"]
+    output_file = settings["output_folder"] + settings["output_file"]
     args = parser.parse_args()
 
     calendar = get_calendar_data(args.url)
@@ -140,7 +148,7 @@ def main():
     shutil.copytree(settings["static_folder"], settings["output_folder"], dirs_exist_ok=True)
 
     # render and write file
-    with open(output_file,'w+', encoding = "utf-8") as f:
+    with open(output_file, 'w+', encoding="utf-8") as f:
         f.write(render(get_template_data(), calendar, settings))
     print(f"Finished Generation.")
 
